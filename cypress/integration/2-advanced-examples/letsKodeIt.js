0d3fa18795ca5772aec511e2c45c6d93ec7663ec
Vedant.Jagani <vedant.jagani@campingworld.com>
2021-09-16 11:22:10 -0500

    changed URL link

--- cypress/integration/2-advanced-examples/letsKodeIt.js
+++ cypress/integration/2-advanced-examples/letsKodeIt.js
@@ -1,17 +1,30 @@
 /// <reference types="cypress" />
 
 it('OpenWebSite',()=>{
-        cy.visit('http://automationpractice.com/index.php') 
+        cy.visit('http://demo.automationtesting.in/Index.html') 
 })
-it('Click Login Btn',()=>{
-    cy.get('.login').click()
-})
-
-it('Enter Email',()=>{
-    cy.get('#email_create').type('test1@gmail.com')
+it('Click Skip Sign In',()=>{
+    cy.get('[id=btn2]').click()
 })
 
-it('Click Create Account',()=>{
-    cy.get('#SubmitCreate > span').click()
+it('Full Name',()=>{
+    cy.get(':nth-child(1) > :nth-child(2) > .form-control').type('VEDANT')
+})
+
+it('Last Name',()=>{
+    cy.get(':nth-child(1) > :nth-child(3) > .form-control').type('JAGANI')
+})
+
+it('Address field',()=>{
+    cy.get('.col-md-8 > .form-control').type('1001 Chicago Ln')
+})
+
+it('Email field',()=>{
+    cy.get('#eid > .form-control').type('testemail@gmail.com')
+})
+
+it('Phone field',()=>{
+    cy.get(':nth-child(4) > .col-md-4 > .form-control').type('1112221234')
+    cy.get(':nth-child(1) > .ng-pristine').check()
     
 })
